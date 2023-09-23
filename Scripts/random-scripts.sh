f6af86efd2855cfa3dfd804f68e96427b2c24a8f
Kristian <kristiankassoemeier@gmail.com>
2023-09-23 14:37:30 +0200

    update random scripts

--- Scripts/random-scripts.sh
+++ Scripts/random-scripts.sh
@@ -7,3 +7,17 @@ ssh -i ~/.ssh/mtckey ec2-user@$DEV_IP
 
 # user_data_log
 sudo cat /var/log/cloud-init-output.log
+
+sed -i '' '45d' /Users/kristianmeier/.ssh/known_hosts
+rm /Users/kristianmeier/.ssh/known_hosts
+nano /Users/kristianmeier/.ssh/known_hosts
+
+lsof -i
+kill -9 <PID>
+
+# Docker
+docker rmi $(docker images -q) -f
+docker system prune
+docker ps -a
+ENV=dev docker-compose up
+docker-compose down --rmi all -v
